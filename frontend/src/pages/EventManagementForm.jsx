67bac8661eb2d4adbac89f2d00f7c69a60946a54
JosephOnwukwe <159090955+JosephOnwukwe@users.noreply.github.com.>
2025-09-19 15:10:44 -0500

    Starting Event Management Form

--- frontend/src/pages/EventManagementForm.jsx
+++ frontend/src/pages/EventManagementForm.jsx
@@ -1,6 +1,20 @@
 function EventManagementForm() {
   return (
-    <div>Event Management Form</div>
+    <div>
+      <h1>Event Management Form</h1>
+      <form>
+        <label style={{display:"block"}}>Name</label>
+        <input
+          type="text"
+          required
+          style={{display:"block"}}
+        />
+        <label style={{display:"block"}}>Description</label>
+        <textarea style={{display:"block"}}></textarea>
+        <button>Add Event</button>
+        <button>Cancel</button>
+      </form>
+    </div>
   )
 }
 
