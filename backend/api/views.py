3645e7b720170769bd4a48f884d871b41f52ead0
JosephOnwukwe <159090955+JosephOnwukwe@users.noreply.github.com.>
2025-10-13 17:32:50 -0500

    set up api app

---
+++ backend/api/views.py
@@ -0,0 +1,39 @@
+# from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+import json
+
+# Create your views here.
+
+@api_view(["GET"])
+def get_events(request):
+    # Hardcoded events
+    events = [
+        {
+            "name": "Soup Kitchen",
+            "description": "Giving food to the homeless",
+            "location": "Some building",
+            "address": "1234 Main St.",
+            "city": "Houston",
+            "state": "TX",
+            "zipCode": "77204",
+            "requiredSkills": ["Food prep", "Leadership"],
+            "urgency": "Medium",
+            "date": "2025-10-16"
+        },
+        {
+            "name": "Plant Trees",
+            "description": "Planting trees for the environment :D",
+            "location": "Pleasant Park",
+            "address": "6767 Jefferson St.",
+            "city": "Houston",
+            "state": "TX",
+            "zipCode": "77203",
+            "requiredSkills": ["Able to lift more than 50 lbs"],
+            "urgency": "Low",
+            "date": "2025-10-15"  
+        }
+    ]
+    data = json.dumps(events)
+    return Response(data, status=status.HTTP_200_OK)
