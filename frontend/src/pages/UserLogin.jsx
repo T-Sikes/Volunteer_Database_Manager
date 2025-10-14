560a27dd9f465588229ab4ff956b091aa21bb39f
EdwardVNguyen <edwardnguyen568@gmail.com>
2025-10-13 23:23:39 -0500

    refactored code, added userLogin folder with components for userLogin, refactored UserLogin.jsx to utilize components, removed UserLogin.css and implemented tailwind css in its stead.

--- frontend/src/pages/UserLogin.jsx
+++ frontend/src/pages/UserLogin.jsx
@@ -1,48 +1,38 @@
 import { useState } from "react";
-import './UserLogin.css'
 
 import email_icon from '../Assets/email.png'
 import password_icon from '../Assets/password.png'
 
+import Input from '../components/UserLogin/Input'
+import SignUpOrIn from '../components/UserLogin/SignUpOrIn'
+
 function UserLogin() {
 
   const [action,setAction] = useState("Login");
-
+  
+  const loginColor = "!bg-[#3fA2A5] hover:!bg-[#203e3f] text-white w-25 h-10 !rounded-full";
+  const registerColor = "!bg-transparent hover:!bg-[#3fA2A5] text-[#3fA2A5] hover:text-white !font-semibold w-25 h-10 !rounded-full border !border-[#3fA2A5] !hover:border-transparent";
+  
   return(
-   <div className='container'>
+    <>
+  <div className='h-screen w-screen flex items-center justify-center'>
+   <div className='flex flex-col items-center p-4 bg-white'>
 
-    <div className="header">
-      <div className="text">{action}</div>
+   <div className="text-[#3fA2A5] text-xl">{action}</div>
+
+   <Input name={email_icon} alternative="email icon" type="email" placeholder="Email" id="email_id"/> 
+   <Input name={password_icon} alternative="password icon" type="password" placeholder="Password" id="password_id"/> 
+
+   <SignUpOrIn switchMode={action}/>
+        
+    <div className='flex flex-row gap-5'>
+      <button className={action==="Login"? loginColor : registerColor } onClick={()=>{setAction("Register")}}>Register</button>
+      <button className={action==="Register"? loginColor : registerColor } onClick={()=>{setAction("Login")}}>Login</button>
     </div>
 
-    <div className="inputs">
-      <div className="input">
-        <img src={email_icon} alt="email icon" />
-        <input type="email" placeholder="Email"/>
-      </div>
-
-      <div className="input">
-        <img src={password_icon} alt="password icon" />
-        <input type="password" placeholder="Password"/>
-      </div>
-
-    </div>
-    
-    {action==="Register"?
-      <div>
-        <div className="signUp">Sign up</div>
-      </div>
-      : 
-      <>
-      <div className="forgot-password">Forgot Password?<span> Click here!</span></div> 
-      <div className = "signIn">Sign in</div> 
-      </>
-    }
-         <div className="submit-container">
-      <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Register")}}>Register</div>
-      <div className={action==="Register"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
-    </div>
    </div>
+  </div>
+    </>
   )
 }
 
