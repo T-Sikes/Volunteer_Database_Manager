37c7669e184b5b67096863e49e1f682b08f1f72a
JosephOnwukwe <159090955+JosephOnwukwe@users.noreply.github.com.>
2025-11-08 19:19:31 -0600

    Reapply "Merge pull request #15 from T-Sikes/Backend_volunteermatching"
    This reverts commit 84d1c076bd44f7315b96d74a3219c749184bf885.

--- backend/event/urls.py
+++ backend/event/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import get_events, get_volunteers, match_volunteers, get_all_matches, get_notifications, send_notification, create_event, update_or_delete_event
+from .views import get_events, get_volunteers, match_volunteers, get_all_matches, get_notifications, send_notification, create_event, update_or_delete_event, delete_match,
 
 urlpatterns = [
     path("", get_events, name="get_events"),
@@ -8,6 +8,7 @@ urlpatterns = [
     path("volunteers/", get_volunteers),   
     path("match/", match_volunteers),      
     path("matches/", get_all_matches),     
+    path("match/<str:volunteer_name>/", delete_match, name="delete_match"),
     path("notifications/", get_notifications),       
     path("send-notification/", send_notification),  
 ]
